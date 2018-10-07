92d9a949557b92d00c22ec1ed5410f52e1d98cb3
Moon Child <kaitline.moon@gmail.com>
2018-10-07 08:19:38 -0400

    Moved input variable inside functions and it worked!

--- liri.js
+++ liri.js
@@ -5,12 +5,14 @@ var request = require("request");
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
-var songName = process.argv[2];
-var artist = process.argv[3];
+var command = process.argv[2];
+
+
 
 // Accessing the Spotify API to retreive song information
-function searchSpotify (songName) {
-    spotify.search({type: 'track', query: songName}, function(error, data) {
+function searchSpotify (input) {
+    var input = process.argv[3];
+    spotify.search({type: 'track', query: input}, function(error, data) {
         if (error) {
           console.log("An error occurred: " + error);
         } else {
@@ -25,16 +27,56 @@ function searchSpotify (songName) {
 
 
 // Accessing the Bands in Town API to get concert information
-function searchBIT (artist) {
-    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, response) {
+function searchBIT (input) {
+    var input = process.argv[3];
+    request("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp", function(error, response) {
         if (error) {
             console.log("An error occured: " + error)
         } else {
             console.log(
                 "Venue name: " + response.venue.name +
-                "Location: " + response.venue.city
-            )
-        }
+                "Location: " + response.venue.city +
+                "Event Date: " + response.datetime
+            );
+        };
     })
 };
 
+// Accessing the OMDB API to get movie information
+function searchOMDB (input) {
+    var input = process.argv[3];
+    request("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+        if (error) {
+            console.log("An error occured: " + error)
+        } else {
+            // console.log(response);
+            console.log(
+                "Movie title: " + JSON.parse(body).Title +
+                "\nReleased: " + JSON.parse(body).Year +
+                "\nIMDB Rating: " + JSON.parse(body).imdbRating +
+                "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings
+            )
+        }
+    })
+}
+
+// Creating a switch statement to loop through functions 
+switch (command) {
+    case "spotify-this-song":
+    searchSpotify();
+    break;
+
+    case "concert-this":
+    searchBIT();
+    break;
+
+    case "movie-this":
+    searchOMDB();
+    break;
+
+    case "do-what-it-says":
+    searchSpotify();
+    break;
+}
+
+console.log(process.argv);
